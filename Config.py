24b766a09234bea83a087bfe79784e1121d94e06
Vladislav1306 <70974645+Vladislav1306@users.noreply.github.com>
2024-02-19 10:32:52 +0300

    Update Config.py

--- Config.py
+++ Config.py
@@ -1,4 +1,4 @@
-TOKEN = "5534218493:AAEDew_MdbDt1d6FXc98Rf1WEo0-QfTcNY4"
+TOKEN = "None"
 urlCal = "https://priem.mai.ru/calendar/?referer=https%3A%2F%2Fyandex.ru%2F#"
 chromedriver = "chromedriver_win32/chromedriver.exe"
 urlUGE = 'https://priem.mai.ru/bachelor/programs/'
